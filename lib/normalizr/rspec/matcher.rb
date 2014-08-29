a853a59e884476ab8686a63a26db53f1d03661d5
Dimko <deemox@gmail.com>
2014-08-30 00:43:36 +0400

    refactor rspec matcher a bit

--- lib/normalizr/rspec/matcher.rb
+++ lib/normalizr/rspec/matcher.rb
@@ -5,7 +5,7 @@ module Normalizr
         Normalization.new(attribute)
       end
 
-      alias_method :normalize_attribute, :normalize
+      alias normalize_attribute normalize
 
       class Normalization
         def initialize(attribute)
@@ -14,15 +14,15 @@ module Normalizr
         end
 
         def description
-          "normalize #{@attribute} from #{@from.nil? ? 'nil' : "\"#{@from}\""} to #{@to.nil? ? 'nil' : "\"#{@to}\""}"
+          "normalize #{@attribute} from #{display(@from)} to #{display(@to)}"
         end
 
         def failure_message
-          "#{@attribute} did not normalize as expected! \"#{@subject.send(@attribute)}\" != #{@to.nil? ? 'nil' : "\"#{@to}\""}"
+          "#{@attribute} did not normalize as expected! #{display(@subject.send(@attribute))} != #{display(@to)}"
         end
 
         def failure_message_when_negated
-          "expected #{@attribute} to not be normalized from #{@from.nil? ? 'nil' : "\"#{@from}\""} to #{@to.nil? ? 'nil' : "\"#{@to}\""}"
+          "expected #{@attribute} to not be normalized from #{display(@from)} to #{display(@to)}"
         end
 
         alias negative_failure_message failure_message_when_negated
@@ -41,6 +41,12 @@ module Normalizr
           @subject = subject
           @subject.send("#{@attribute}=", @from)
           @subject.send(@attribute) == @to
+        end
+
+        protected
+
+        def display(value)
+          value.nil? ? 'nil' : "\"#{value}\""
         end
       end
     end
